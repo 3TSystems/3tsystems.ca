4752b5fbe7d49eb3dd25c1e2951569bb0a2a068b
Jordan Hiebert <hiebert@gmail.com>
2015-04-07 11:19:50 -0500

    Touch up mailer for formatting in Zendesk

--- site/mail.php
+++ site/mail.php
@@ -12,12 +12,7 @@ $headers = "From: 3T Systems <website@3tsystems.ca>\r\n";
 $headers .= "Reply-to: $name <$email>\r\n";
 $headers .= "X-Mailer: PHP/".phpversion();
 
-$formcontent = "From:\r\n$name\r\n\r\n";
-$formcontent .= "E-mail:\r\n$email\r\n\r\n";
-$formcontent .= "Subect:\r\n$subject\r\n\r\n";
-$formcontent .= "Message:\r\n$message";
-// make sure each line doesn't exceed 70 characters
-$formcontent = wordwrap($formcontent, 70);
+$formcontent = "$message";
 
 mail($mail_to, $mail_subject, $formcontent, $headers);
 header('Location: thanks.html');
